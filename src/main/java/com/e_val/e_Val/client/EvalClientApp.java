7be36cbc3ac622adef79116099c4bbf9ca0ca24c
Hussain Ali Zaidi <hussainalizaidi32@gmail.com>
2025-03-23 17:31:21 +0500

    Demo UI

--- src/main/java/com/e_val/e_Val/client/EvalClientApp.java
+++ src/main/java/com/e_val/e_Val/client/EvalClientApp.java
@@ -1,19 +1,36 @@
 package com.e_val.e_Val.client;
 
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 public class EvalClientApp extends Application {
+    private static ConfigurableApplicationContext springContext;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
+        // Start Spring Boot in a background thread
+        new Thread(() -> {
+            springContext = SpringApplication.run(com.e_val.e_Val.EValApplication.class);
+        }).start();
+
+        // Load JavaFX UI
         Parent root = FXMLLoader.load(getClass().getResource("/client/views/login.fxml"));
-        primaryStage.setTitle("e-Val Quiz System");
+        primaryStage.setTitle("e-Val Login");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() {
+        // Shutdown Spring Boot when JavaFX exits
+        if (springContext != null) {
+            springContext.close();
+        }
     }
 
     public static void main(String[] args) {
